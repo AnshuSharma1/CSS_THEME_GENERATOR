984f7f51f28c022206a47111f8b4f0cc0eccf3b4
Anshu <sharmanshu96@gmail.com>
2017-07-09 02:01:29 +0530

    Layout 1 file created

---
+++ Layout1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<head>
+		<title>Layout 1</title>
+	</head>
+<body>
+
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
